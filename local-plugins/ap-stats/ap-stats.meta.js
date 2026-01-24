74a5a56a1587099ccffb6a03b58f507afa444672
mordenkainennn <watson.public@gmail.com>
2026-01-25 03:19:31 +0800

    feat(ap-stat):计算machina阵营产生的ap

---
+++ local-plugins/ap-stats/ap-stats.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id             iitc-plugin-ap-stats@mordenkainennn
+// @name           Available AP statistics [mordenkainennn]
+// @category       Info
+// @version        0.4.6.1
+// @namespace      https://github.com/mordenkainennn/ingress-intel-total-conversion
+// @updateURL      https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/main/local-plugins/ap-stats.meta.js
+// @downloadURL    https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/main/local-plugins/ap-stats.user.js
+// @description    Displays the per-team AP gains available in the current view. [mordenkainennn-tweak: Standalone script conversion]
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @author         Hollow011, mordenkainennn
+// @grant          none
+// ==/UserScript==
