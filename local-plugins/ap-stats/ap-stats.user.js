b4be80fbde0a13dbe97042ce46279aa7d045169c
mordenkainennn <watson.public@gmail.com>
2026-02-20 13:10:14 +0800

    fix(ap-stats):fix userscript

--- local-plugins/ap-stats/ap-stats.user.js
+++ local-plugins/ap-stats/ap-stats.user.js
@@ -4,9 +4,9 @@
 // @category       Info
 // @version        0.4.6.2
 // @namespace      https://github.com/mordenkainennn/ingress-intel-total-conversion
-// @updateURL      https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/master/local-plugins/ap-stats.meta.js
-// @downloadURL    https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/master/local-plugins/ap-stats.user.js
-// @description    Displays the per-team AP gains available in the current view. [mordenkainennn-tweak: Standalone script conversion]
+// @updateURL      https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/master/local-plugins/ap-stats/ap-stats.meta.js
+// @downloadURL    https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/master/local-plugins/ap-stats/ap-stats.user.js
+// @description    Displays the per-team AP gains available in the current view. [cloverjun-tweak: Standalone script conversion]
 // @match          https://intel.ingress.com/*
 // @match          https://intel-x.ingress.com/*
 // @author         Hollow011, cloverjune
