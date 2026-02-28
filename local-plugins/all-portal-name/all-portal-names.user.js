b2bcf36ce61cebb860d0f26ce0a67e335552c603
mordenkainennn <watson.public@gmail.com>
2026-02-28 23:34:46 +0800

    fix(all-portal-names):update userscript

--- local-plugins/all-portal-name/all-portal-names.user.js
+++ local-plugins/all-portal-name/all-portal-names.user.js
@@ -5,7 +5,9 @@
 // @version        0.5.0
 // @description    Show all portal names on the map. Includes a setting to show all names regardless of overlap at a certain zoom level.
 // @id             all-portal-names
-// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @namespace      https://github.com/mordenkainennn/ingress-intel-total-conversion
+// @updateURL      https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/master/local-plugins/all-portal-name/all-portal-names.meta.js
+// @downloadURL    https://github.com/mordenkainennn/ingress-intel-total-conversion/raw/master/local-plugins/all-portal-name/all-portal-names.user.js
 // @match          https://intel.ingress.com/*
 // @match          https://intel-x.ingress.com/*
 // @grant          none
