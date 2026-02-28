bd8c0d378c2bff1649371d3e3f61091a6cbfc9e3
mordenkainennn <watson.public@gmail.com>
2026-02-28 23:59:47 +0800

    maintenance: minor adjustment

--- local-plugins/all-portal-names/all-portal-names.user.js
+++ local-plugins/all-portal-names/all-portal-names.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @author         ZasoGD (+ modified by cloverjune)
-// @name           IITC plugin: All Portal Names
+// @name           All Portal Names
 // @category       Layer
 // @version        0.5.0
 // @description    Show all portal names on the map. Includes a setting to show all names regardless of overlap at a certain zoom level.
